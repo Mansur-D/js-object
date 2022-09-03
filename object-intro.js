0d43fe33f4f0c12ea9213d7ad8c2b63d64e47c2c
Mansur <mansurdzukalaev@gmail.com>
2022-09-03 11:08:05 +0300

    Объекты 2 день 2 неделя

--- object-intro.js
+++ object-intro.js
@@ -1,1 +1,23 @@
 //
+let start = {
+    name:'mansur',
+    age:26,
+};
+
+let person = {
+    firstname: 'mansur',
+    lastname: 'dzhukalaev',
+    city: 'Alkhazurovo',
+    age: 26,
+    height: 182,
+    heightMeters: '1.82',
+    haveCar: false,
+    carName: undefined,
+    mobilePhone: {
+        MobileName: 'samsung',
+        price: 8000,
+        operator: 'beeline',
+    },
+    };
+
+    
